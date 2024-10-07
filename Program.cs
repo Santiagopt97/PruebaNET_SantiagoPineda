aed662f3adbe03746089c0863a14e4b980644088
Santiagopt97 <santiagopineda99@gmail.com>
2024-10-07 11:44:47 -0500

    Fic: config swagger

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using DotNetEnv;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using PruebaNET_SantiagoPineda.Config;
 using PruebaNET_SantiagoPineda.Data;
 using PruebaNET_SantiagoPineda.Repositories;
