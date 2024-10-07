0dde35a1e540292964c3374f368a8a9e5874029e
Santiagopt97 <santiagopineda99@gmail.com>
2024-10-07 08:27:29 -0500

    Fix: typeof capacity

---
+++ Migrations/20241007132452_FixRoom.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PruebaNET_SantiagoPineda.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixRoom : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "max_occupancy_person",
+                table: "rooms",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "tinyint unsigned");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte>(
+                name: "max_occupancy_person",
+                table: "rooms",
+                type: "tinyint unsigned",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
