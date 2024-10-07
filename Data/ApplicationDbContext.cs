bffb9e32ab926a3b37de12d5e1d76d58eaf6827f
Santiagopt97 <santiagopineda99@gmail.com>
2024-10-07 07:36:14 -0500

    Feat: add Dbset for migrations

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using PruebaNET_SantiagoPineda.Models;
 
 namespace PruebaNET_SantiagoPineda.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<RoomType> RoomTypes  { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Guest> Guests { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
